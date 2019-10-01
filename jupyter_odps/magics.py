d1d544dfe543f0dd763bdde592fe55834403b21c
Masahiro Ishii <marufeuille@gmail.com>
2019-10-01 10:30:02 +0000

    display result with DataFrame(DESC and INSERT only)

--- jupyter_odps/magics.py
+++ jupyter_odps/magics.py
@@ -3,6 +3,7 @@ from IPython.core.magic import (register_line_magic, register_cell_magic)
 from io import StringIO
 
 from odps import ODPS
+import pandas as pd
 
 @magics_class
 class ODPSMagic(Magics):
@@ -13,13 +14,51 @@ class ODPSMagic(Magics):
 
     @cell_magic
     def odps(self, line, cell):
-        sio = StringIO(cell).getvalue()
+        sql = StringIO(cell).getvalue()
 
         result = []
-        with self.odps.execute_sql(sio).open_reader() as reader:
-            for record in reader:
-                result.append(record)
-        return result
+        
+        column = []
+        fields = []
+
+        with self.odps.execute_sql(sql).open_reader() as reader:
+            if sql[0:4].upper() == "DESC":
+                field_flag = False
+                idx = 1
+                for record in reader:
+                    for field in record:
+                        if field[1][0:2] == "+-":
+                            continue
+                        if field_flag:
+                            x = field[1][1:-1].split("|")
+                            column.append("FieldName-{}".format(idx))
+                            fields.append(x[0].strip())
+                            column.append("FieldType-{}".format(idx))
+                            fields.append(x[1].strip())
+                            idx += 1
+                        else:
+                            for item in field[1][1:-1].strip().split("|"):
+                                x = item.split(":", 1)
+                                if x[0].strip() == "Field":
+                                    field_flag = True
+                                    break
+                                if len(x) == 1:
+                                    continue
+                                column.append(x[0].strip())
+                                fields.append(x[1].strip() if len(x) == 2 else "")
+                return pd.DataFrame([fields], columns=column)
+            else:
+                for record in reader:
+                    c = []
+                    f = []
+                    for field in record:
+                        c.append(field[0])
+                        f.append(field[1])
+                    if len(column) == 0:
+                        column = c
+                    fields.append(f)
+        
+        return pd.DataFrame(fields, columns=column)
     
 def load_ipython_extension(ipython):
     params = {}
